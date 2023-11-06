4948d99ec2ebdc2735e99552a593e88454abc91c
Jean-Christophe Morin <jcmorin@anaconda.com>
2023-11-06 09:53:10 -0500

    Simplify mkl test code

--- recipe/tests/mkl-devel/app.c
+++ recipe/tests/mkl-devel/app.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 #include <mkl.h>
 
-void main(void) {
-    int my_cbwr_branch;
-    /* Align all input/output data on 64-byte boundaries */
-    /* "for best performance of Intel® oneAPI Math Kernel Library */
-    void *darray;
-    int darray_size=1000;
-    /* Set alignment value in bytes */
-    int alignment=64;
-    /* Allocate aligned array */
-    darray = mkl_malloc (sizeof(double)*darray_size, alignment);
-    /* Find the available MKL_CBWR_BRANCH automatically */
-    my_cbwr_branch = mkl_cbwr_get_auto_branch();
-    /* User code without oneMKL calls */
-    /* Piece of the code where CNR of oneMKL is needed */
-    /* The performance of oneMKL functions might be reduced for CNR mode */
-    /* If the "IF" statement below is commented out, Intel® oneAPI Math Kernel Library will run in a regular mode, */
-    /* and data alignment will allow you to get best performance */
-    if (mkl_cbwr_set(my_cbwr_branch)) {
-        printf("Error in setting MKL_CBWR_BRANCH! Aborting…\n");
-        return;
-    }
-    /* CNR calls to oneMKL + any other code */
-    /* Free the allocated aligned array */
-    mkl_free(darray);
+int main(void) {
+    printf("MKL max threads: %d\n", mkl_get_max_threads());
+    return 0;
 }
